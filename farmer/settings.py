eaecddbdab526d5678edf7f04f4eb703d2c13994
siddharth tiwari <siddharthtiwarikreplind@gmail.com>
2022-09-09 19:44:47 +0530

    changes

--- farmer/settings.py
+++ farmer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(r=@7747^bg&beqxfezdgnt^9#mlk+x4njci=&^51a$hz&80b*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
